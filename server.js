84fca47e211aab433ef19a1592af3d90be1a1d8c
adhanu99 <145645386+adhanu99@users.noreply.github.com>
2026-04-05 18:51:52 +0530

    Update server.js 3

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.get("/stream", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
